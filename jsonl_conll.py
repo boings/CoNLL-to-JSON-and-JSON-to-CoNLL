fc67b7c48ad229630f731bf9314b0ff36d22bc8d
boings <freakflak@gmail.com>
2023-06-05 22:51:12 -0400

    jsonl to conll HTML initial implementation.

--- jsonl_conll.py
+++ jsonl_conll.py
@@ -1,8 +1,7 @@
-import json
 import glob
 import os 
 import sys
-import csv
+import jsonlines
 
 #NOTES: 
 #usage command line: python json_conll.py input_folder
@@ -14,7 +13,7 @@ def get_paths(input_folder):
     :param inputfolder: inputfolder used in main
     """
     list_files = []
-    conll_folder = glob.glob(input_folder + '/*.json')
+    conll_folder = glob.glob(input_folder + '/*.jsonl')
     
     for filename in conll_folder:
         list_files.append(filename)
@@ -27,9 +26,8 @@ def load_text(txt_path):
     Returns a string
     :param txt_path: list with filepaths
     """
-    with open(txt_path, 'r') as json_file:
-        data = json_file.read()
-        content = json.loads(data)
+    with jsonlines.open(txt_path, 'r') as json_file:
+        content = [json_obj for json_obj in json_file]
     
     return content
 
@@ -47,24 +45,26 @@ def process_and_write(loaded_dicts, input_folder, text):
         os.makedirs(directory)
     
     #get basename of path and change extension to '.conll'
-    base = os.path.basename(text)[:-5]
+    base = os.path.basename(text)[:-6]
     conll_str = '.conll'
     basename = base + conll_str
     
-    #add directory with files to the input folder
-    path = os.path.join(input_folder, directory)
-    complete_name = os.path.join(path, basename)
+    # add directory with files to the input folder
+    complete_name = os.path.join(directory, basename)
     
-    #open write file
-    f = csv.writer(open(complete_name, 'w'), delimiter=(' '))
-    
-    #for every value in the json dict, add values to list and write list
-    for json_dict in loaded_dicts:
-        values_list = []
-        for key, value in json_dict.items():
-            values_list.append(value)
+    with open(complete_name, 'w', encoding='utf-8') as conll_file:
+        # Process each JSON object in the JSONL file
+        for json_obj in loaded_dicts:
+            for key, value in json_obj.items():
+                contents = value.get('contents', '')
+                tag = value.get('tag', '')
+
+                # Split contents into tokens
+                tokens = contents.split()
                 
-        f.writerow(values_list)
+                # Write each token and tag on a separate line
+                for token in tokens:
+                    conll_file.write(f'{token}\t{tag}\n')
         
 def main():
     
@@ -72,6 +72,7 @@ def main():
     
     txt_path = get_paths(input_folder)
     for text in txt_path:
+        print(text)
         loaded_dicts = load_text(text)
         process_and_write(loaded_dicts, input_folder, text)
 
